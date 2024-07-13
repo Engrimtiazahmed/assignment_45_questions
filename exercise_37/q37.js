e934a515c08079c08aea1ade886d892e94fee8f3
Engrimtiazahmed <engineerimtiaz@yahoo.com>
2024-07-13 21:14:56 +0500

    Completed TS assignment

---
+++ exercise_37/q37.js
@@ -0,0 +1,12 @@
+"use strict";
+/* Qquestion #37;Large Shirts:
+               Modify the make_shirt() function so that shirts are large by default with a message that reads
+               I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt
+               of any size with a different message.
+*/
+function make_shirt(size = "large", msg = "I love Typescript.") {
+    console.log(`Making a ${size} T-Shirt with a massage "${msg}" printed on it.`);
+}
+make_shirt(); // default   message
+make_shirt("medium"); // default message
+make_shirt("small", "Typescript is superset of Javascript"); // With different message
