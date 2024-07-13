200a2d7471ac16633d772436142dd942cc45b6de
Engrimtiazahmed <engineerimtiaz@yahoo.com>
2024-07-13 21:24:42 +0500

    Completed TS Assignment

---
+++ exercise_38/q38.js
@@ -0,0 +1,13 @@
+"use strict";
+/* Question #37;Cities:
+            Write a function called describe_city() that accepts the name of a city and its country. The function
+            should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a
+            default value. Call your function for three different cities, at least one of which is not in the
+            default country.
+*/
+function describe_city(city_name = "Jamshoro", country = "Pakistan") {
+    console.log(`${city_name} is in ${country}.`);
+}
+describe_city();
+describe_city("Karachi");
+describe_city("Paris", "france");
